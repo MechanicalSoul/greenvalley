866f185eb199ed98e8dea7062de906fba35d1c83
Daniil Samylin <mymechanicalsoul@gmail.com>
2017-02-12 15:13:00 +0300

    Правки стилей на booking

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
   $('.page-header__burger').on('click', function(event) {
-  	
+
   	event.preventDefault();
 
   	$('.main-nav').slideToggle('slow');
   });
+
+  // $('.owl-carousel').owlCarousel({
+  // 	loop: true,
+  // 	center: true,
+  // 	items: 3
+  // });
 });
