8973440b8cd092d1d302818972a2be29bf3e35f3
Daniil Samylin <mymechanicalsoul@gmail.com>
2017-02-14 15:48:29 +0300

    Добавил функционал к promo и rest в index

--- src/js/script.js
+++ src/js/script.js
@@ -6,9 +6,20 @@ $(document).ready(function(){
   	$('.main-nav').slideToggle('slow');
   });
 
+  $('.promo__tab').on('click', function(event) {
+
+  	event.preventDefault();
+
+  	if(!$(this).hasClass('promo__tab--active')) {
+	  	$('.promo__tab').toggleClass('promo__tab--active');
+	  	$('.promo__item').toggleClass('promo__item--active');
+	  	$('.rest__item').toggleClass('rest__item--active');
+	  }
+  });
+
   $('.owl-carousel').owlCarousel({
   	loop: true,
   	center: true,
-  	items: 3
+  	items: 1
   });
 });
